26148d74a8b96c35b125ca885486e0001c63262a
arminkhayyer <armin.khayyer@gmail.com>
2018-01-10 23:54:23 -0600

    creating checkout model

--- orders/views.py
+++ orders/views.py
@@ -1,24 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
+from user_profile.views import UserFormView, LoginView
+
+
 
 def order_create(request):
     cart =Cart(request)
-    if request.method == 'POST' :
-        form = OrderCreateForm(request.POST)
-        if form.is_valid():
-            order = form.save()
-            for item in cart :
-                OrderItem.objects.create(order= order,
-                                         drug=item['drug'],
-                                         #price=item['price'],
-                                         quantity=item['quantity'])
+    if request.user.is_authenticated():
+        if request.method == 'POST' :
+                form = OrderCreateForm(request.POST)
+                if form.is_valid():
+                    order = form.save()
+                    for item in cart :
+                        OrderItem.objects.create(order= order,
+                                                 drug=item['drug'],
+                                                 #price=item['price'],
+                                                 quantity=item['quantity'])
 
 
-                cart.clear()
-                return render(request, 'orders/created.html', {'order':order})
+                        cart.clear()
+                        return render(request, 'orders/created.html', {'order':order})
+
+        else:
+            form = OrderCreateForm()
+            return render(request, 'orders/create.html', {'cart': cart, 'form': form} )
 
     else:
-        form = OrderCreateForm()
-        return render(request, 'orders/create.html', {'cart': cart, 'form': form} )
+        return redirect('user_profile:login')

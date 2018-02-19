b5f8d63f702506635ad9949f1307281692391b6d
arminkhayyer <armin.khayyer@gmail.com>
2018-02-19 03:51:03 -0600

    hich gohi nakhordam

--- orders/models.py
+++ orders/models.py
@@ -7,6 +7,12 @@ class Order(models.Model):
     updated = models.DateTimeField(auto_now_add=True)
     paid = models.BooleanField(default=False)
     verified = models.BooleanField(default=False)
+    sent=models.BooleanField(default=False)
+    shiping_method_choices =(
+        ('e == 7.5', 'express one day shiping $7.5'),
+        ('r == 0', 'regular three day shiping'  ),
+    )
+    shiping_method=models.CharField(choices=shiping_method_choices,default='r', max_length=50)
     info = models.ForeignKey(UserInfo,null=True,related_name='info')
     class Meta:
         ordering= ('-created',)
@@ -21,12 +27,12 @@ class Order(models.Model):
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items')
     drug = models.ForeignKey(Drug, related_name='order_items')
-    #price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self):
         return '{}'.format(self.id)
 
-   # def get_cost(self):
-       # return self.price * self.quantity
+    def get_cost(self):
+        return self.price * self.quantity
 

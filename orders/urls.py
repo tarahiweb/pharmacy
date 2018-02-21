f09c1e75b7ce4442cf6c2e339369712402c39210
arminkhayyer <armin.khayyer@gmail.com>
2018-02-21 03:06:15 -0600

    sallam

--- orders/urls.py
+++ orders/urls.py
@@ -7,4 +7,5 @@ urlpatterns= [
     #url(r'^create/$', views.order_create, name='order_create'),
     url(r'^order-info/$', views.order_info, name='order_info'),
     url(r'^checkout/$', views.CheckoutView.as_view(), name='checkout'),
+    url(r'^checkout-successful/$', views.Checkout_Successfull , name='checkout-successful'),
 ]
